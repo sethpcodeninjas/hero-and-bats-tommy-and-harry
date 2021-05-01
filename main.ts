677153de41629f76e63fc12c9c5b63562461fe05
sethpcodeninjas <79381106+sethpcodeninjas@users.noreply.github.com>
2021-05-01 17:03:21 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,51 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.setFlag(SpriteFlag.GhostThroughSprites, true)
+})
+let bat: Sprite = null
+let hero = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(hero)
+// Life and overlap
+info.setLife(3)
+tiles.setTilemap(tilemap`level1`)
+tiles.placeOnTile(hero, tiles.getTileLocation(2, 2))
+// Creating enemies
+game.onUpdateInterval(2000, function () {
+    bat = sprites.create(img`
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c 1 b b b 1 b c b b c c c . . 
+        . c f b b b f b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b 1 f f 1 c b b b b f . . . . 
+        f f 1 f f 1 f b b b b f c . . . 
+        f f 2 2 2 2 f b b b b f c c . . 
+        . f 2 2 2 2 b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `, SpriteKind.Enemy)
+    bat.setPosition(160, randint(0, 120))
+    bat.setVelocity(-50, 0)
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,55 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000200002090909090909090909090909090901060b190b0b0b0b19190b0b19190b0b07180b1b1b1c1c1c1b19191c19190b0b17060b0b191919191b1b1b1c1c190b0b07060c11050505110e0b1b191c1c1c0b07060d10090909100f0b0b19191c1c0b07061b0b0b0b0b0b0b0b1b19191c0b0b07060b0b0b0b0b1b1919190b0b0b0b0b17180b1c1c1c1919190c11050505110e07060b1c0b1c0b0b0b0d09091009100f07060b19190c11050511050e0b0b0b0b07060b0b190d10090910090f0b0b1b1907060b19190b0b19190b19191919190b07060b1b0b1c1c0b1b19190b1b0b0b0b17180b0b0c050505050505050505050e0a06190b0a081009090909100809090f0706190b0a06190b120b19190b150b0b07061b0b0a060b0b0b0b191b0b151b0b07061c0b0d0f09090909010b0b0e191907061c0b160b190b0b0b07141406191b07181c190c0e0b1c0b0b070b0b06191917060b1917060b191c0b071a1c060b1b0a060b1c0a0f0b191919071c0b06191907060b1c170c13130505040b1a06191c07061b1c070a0b0b0b0b0b191906191c07061919070a0b1c191b19190b061c1c07181919170d080808131309090f1c1917060b0b070e0b0b0b0b0b0b0b0b1c1907061b0b07060b1c1c190b0b0b1c1c0b07060b0b17060b0b1c1c1c1c1c1c1b0b07060b0b0d0f0b0b0b0b19190b0b0b0b0703050505050505050505050505050504`, img`
+2222222222222222
+2..............2
+2..............2
+2..............2
+22222222.......2
+22222222.......2
+2..............2
+2..............2
+2.......22222222
+2.......22222222
+2...2222222....2
+2...2222222....2
+2..............2
+2..............2
+2..2222222222222
+2..2222222222222
+2..22..........2
+2..22..........2
+2..2222222..2..2
+2........2222..2
+2..22....2..2..2
+2..22....2..2..2
+2..22....2..2..2
+2..22..222..2..2
+2..22.......2..2
+2..22.......2..2
+2..22222..222..2
+2..22..........2
+2..22..........2
+2..22..........2
+2..22..........2
+2222222222222222
+`, [myTiles.transparency16,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterEast1,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterEast0,sprites.dungeon.floorDark2,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleOuterNorth2,sprites.dungeon.purpleOuterSouth2,sprites.dungeon.chestClosed,sprites.dungeon.stairNorth,sprites.dungeon.stairLadder,sprites.dungeon.stairEast,sprites.dungeon.stairWest,sprites.dungeon.purpleOuterEast2,sprites.dungeon.purpleOuterWest2,sprites.dungeon.floorDark0,sprites.dungeon.floorDark3,sprites.dungeon.floorDark5,sprites.dungeon.floorDark1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
